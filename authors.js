e662cea31c9ae2fb7ee652bf72307b6b77574fa0
Randson Oliveira <randsonjs@gmail.com>
2014-12-15 00:52:39 -0400

    Adiciona template de autores

---
+++ authors.js
@@ -0,0 +1,9 @@
+module.exports = {
+    randsonjs: {
+        name: "Randson Oliveira",
+        bio: "A brazilian software developer who lives in porto velho",
+        gravatar: "0758c245d296a4abd67ac5bfcca75cbd",
+        gplus: "+RandsonOliveiraJarthis",
+        twitter: "randsonjs"
+    }
+}
