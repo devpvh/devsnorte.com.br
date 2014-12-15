6868a0a989fc7784d521c83896316b05950a59e2
Randson Oliveira <randsonjs@gmail.com>
2014-12-15 00:52:26 -0400

    Adiciona helpers, collections e configurações de plugins

--- docpad.js
+++ docpad.js
@@ -1,3 +1,79 @@
 module.exports = {
 
+	// These are variables will be accessible via our templates
+
+	templateData: {
+		authors: require("./authors.js"),
+
+		site: {
+			title: "",
+			description: "Um lugar para discutir sobre as melhores práticas de desenvolvimento",
+			url: "http://devsnorte.com.br",
+			image: "http://devsnorte.com.br/images/logo.png"
+		},
+
+		github: {
+			user: "pvhjs",
+			repo: "devsnorte.com.br"
+		},
+
+		/* Helpers
+		===================================================================== */
+		getAuthor: function(author) {
+			if(author)
+				return this.authors[author];
+
+			return this.authors["Comunidade"];
+		},
+
+		getAuthorImage: function(author) {
+			if(this.authors[author].gravatar)
+				return "https://2.gravatar.com/avatar/" + this.authors[authors].gravatar;
+
+			return this.authors[author].image;
+		},
+
+		getAuthorTwitter: function(author) {
+			return "https://twitter.com/" + this.authors[author].twitter;
+		},
+
+		getGitgubUrl: function() {
+			return "https://github.com/" + this.github.user + "/" + this.github.repo;
+		},
+
+		getGithubEditUrl: function(path) {
+			return this.getGithubUrl() + "/edit/master/src/documents/" + path;
+		},
+
+		getTitle: function() {
+			if(this.document.title)
+				return "" + this.document.title + "&mdash;" + this.site.title;
+
+			return "" + this.site.title;
+		},
+
+		/* Development Environment
+		===================================================================== */
+		environments: {
+			development: {
+				templateData: {
+					site: {
+						url: "http://localhost:9778"
+					}
+				}
+			}
+		},
+
+		/* Collections
+		===================================================================== */
+		collections: {
+
+		},
+
+		/* Plugins Configurations
+		===================================================================== */
+		plugins: {
+
+		}
+	}
 }
