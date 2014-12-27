e693ff67bd2df30f6deb80b4ed2e76dc7f26fd4d
Randson Oliveira <randsonjs@gmail.com>
2014-12-27 03:16:22 -0400

    Cria estrutura das páginas

---
+++ src/files/js/scripts.js
@@ -0,0 +1,10 @@
+(function() {
+    var gcse = document.createElement('script');
+
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=016164318343189681230:csnnqlpzqjg';
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+}());
